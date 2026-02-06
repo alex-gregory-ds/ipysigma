3f92b040dea7b9110e5243e06aba4c4f5f088a7c
Alex Gregory <alex.gregory1@outlook.com>
2026-02-06 10:23:21 +0000

    Add comment to file empty file to make it not empty

---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+"""This comment is here so the file is not empty.
+
+The ipysigma build seems to rely on this file but setuptools (or something)
+will not copy empty files resulting in missing file errors.
+"""
